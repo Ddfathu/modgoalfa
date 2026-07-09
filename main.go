1ed2c5b68e7c65b3ed3f1d43e12d5b77b58fd93a
Ddfathu <108730817+Ddfathu@users.noreply.github.com>
2026-07-09 11:14:00 +0700

    Manage via Bot

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
@@ -17,7 +16,6 @@ import (
 const WS_MAGIC = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
 var (
-	// Alpine-friendly port binding (menggunakan port internal proxy)
 	listenPort = getEnv("WS_PORT", "8880")
 	targetHost = getEnv("WS_TARGET_HOST", "127.0.0.1")
 	targetPort = getEnv("WS_TARGET_PORT", "22")
@@ -53,7 +51,6 @@ func tuneSocket(conn net.Conn) {
 func main() {
 	log.Printf("[go-ws] Memulai Turbo WS Proxy pada port %s -> SSH %s\n", listenPort, targetPort)
 	
-	// Binding tanpa menentukan IP "0.0.0.0" agar dual-stack IPv4/IPv6 Alpine aktif otomatis
 	listener, err := net.Listen("tcp", ":"+listenPort)
 	if err != nil {
 		log.Fatalf("[go-ws] Gagal binding port: %v", err)
@@ -73,7 +70,6 @@ func main() {
 func handleWebSocket(clientConn net.Conn) {
 	defer clientConn.Close()
 
-	// Baca jabat tangan HTTP/WS Custom
 	clientConn.SetReadDeadline(time.Now().Add(3 * time.Second))
 	headerBuf := make([]byte, 16384)
 	n, err := clientConn.Read(headerBuf)
@@ -103,11 +99,9 @@ func handleWebSocket(clientConn net.Conn) {
 	h.Write([]byte(wsKey + WS_MAGIC))
 	acceptKey := base64.StdEncoding.EncodeToString(h.Sum(nil))
 
-	// Respon standard HTTP murni tanpa masking biner palsu
 	response := fmt.Sprintf("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: %s\r\n\r\n", acceptKey)
 	clientConn.Write([]byte(response))
 
-	// Dial ke Dropbear internal port 22
 	sshConn, err := net.Dial("tcp", targetHost+":"+targetPort)
 	if err != nil {
 		return
